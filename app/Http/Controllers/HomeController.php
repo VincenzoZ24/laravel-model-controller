3f3c65b1986d9cb113bfb47eb4f86c58c17e8fa8
VincenzoZ24 <zitov287@gmail.com>
2023-01-16 16:50:28 +0100

    stampati titoli in pagina senza stile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function home(){
+        $movies = Movie::all();
+        
+        return view('guest.home', [
+            'movies' => $movies,
+        ]);
+
+    }
 }
